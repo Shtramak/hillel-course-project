75fdfa062afd544ec82b28964ce76292381f0280
Heryv1m <podalead@gmail.com>
2018-02-17 23:04:01 +0200

    Prometey >> Heryv1m > Mod: AbstractDao

--- education/src/main/java/com/courses/tellus/dao/AbstractDao.java
+++ education/src/main/java/com/courses/tellus/dao/AbstractDao.java
@@ -7,7 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
-public abstract class AbstractController <E, K> {
+public abstract class AbstractDao<E, K> {
 
     public Connection conn = ConnectionUtils.getConnection();
 
