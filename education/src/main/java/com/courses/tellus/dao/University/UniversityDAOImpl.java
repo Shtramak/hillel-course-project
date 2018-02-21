9861811a117b5e108d28a1d24fab82847a8ca45e
Stanislav Zabramnyi <stas.zabramnyi@gmail.com>
2018-02-21 23:16:12 +0200

    Fix UniversityImpl

--- education/src/main/java/com/courses/tellus/dao/University/UniversityDAOImpl.java
+++ education/src/main/java/com/courses/tellus/dao/University/UniversityDAOImpl.java
@@ -11,30 +11,20 @@ import java.util.List;
 public class UniversityDAOImpl implements AbstractDao {
     private Connection connection = ConnectionUtils.getConnection();
 
- public  static University getResultUniversity(PreparedStatement preparedStatement){
-
-     PreparedStatement ps = preparedStatement;
-     University university = new University();
-     try(ResultSet rSet=ps.executeQuery()) {
-         while (rSet.next()) {
-             university.setId(rSet.getInt("id"));
-             university.setNameOfUniversity(rSet.getString("name"));
-             university.setAddress(rSet.getString("address"));
-             university.setSpecialization("specialization");
- }}
-
- catch (SQLException e){
-         e.printStackTrace();
- }return  university;
- }
-
     @Override
     public List<University> getAll()  {
         List<University> listOfUniversities = new ArrayList<University>();
         try (PreparedStatement ps=connection.prepareStatement("SELECT*FROM Universities")){
 
-            listOfUniversities.add( getResultUniversity(ps));
-
+            try(ResultSet rSet=ps.executeQuery()) {
+                while (rSet.next()) {
+                    University university = new University();
+                    university.setId(rSet.getInt("id"));
+                    university.setNameOfUniversity(rSet.getString("nameOfUniversity"));
+                    university.setAddress(rSet.getString("address"));
+                    university.setSpecialization(rSet.getString("specialization"));
+                    listOfUniversities.add(university);
+                }}
         } catch (SQLException e){
             e.printStackTrace();
         }
@@ -51,7 +41,16 @@ public class UniversityDAOImpl implements AbstractDao {
             idForSearch = (int)id;
             ps.setInt(1,idForSearch);
             ps.executeQuery();
-            listOfUniversitiesById.add(getResultUniversity(ps));
+            try(ResultSet rSet=ps.executeQuery()) {
+                while (rSet.next()) {
+                    University university = new University();
+                    university.setId(rSet.getInt("id"));
+                    university.setNameOfUniversity(rSet.getString("nameOfUniversity"));
+                    university.setAddress(rSet.getString("address"));
+                    university.setSpecialization(rSet.getString("specialization"));
+                    listOfUniversitiesById.add(university);
+                    listOfUniversitiesById.add(university);}}
+
         } catch (SQLException | ClassCastException e){
             e.printStackTrace();
         }
@@ -62,20 +61,16 @@ public class UniversityDAOImpl implements AbstractDao {
     @Override
     public void update(Object entity) {
         University u;
-        int idForUpdate;
-        try (PreparedStatement ps= connection.prepareStatement
-                ("UPDATE Univarsities SET" + " nameOfUniversity = ? , address =?, specialization =?" +
-                        "WHERE id= ?")) {
 
+        try (PreparedStatement ps= connection.prepareStatement
+                ("UPDATE Universities SET" + " nameOfUniversity = ? , address =?, specialization =?" +
+                        "WHERE id= ?")) {
             u = (University) entity;
-            idForUpdate = u.getId();
-            String nameOfUniversity = u.getNameOfUniversity();
-            String address = u.getAddress();
-            String specialization = u.getSpecialization();
-            ps.setString(1, nameOfUniversity);
-            ps.setString(2, address);
-            ps.setString(3, specialization);
-            ps.setInt(4,idForUpdate);
+
+            ps.setString(1, u.getNameOfUniversity());
+            ps.setString(2, u.getAddress());
+            ps.setString(3, u.getSpecialization());
+            ps.setInt(4,u.getId());
             ps.executeUpdate();
 
         } catch (SQLException | ClassCastException e) {
@@ -102,23 +97,22 @@ public class UniversityDAOImpl implements AbstractDao {
 
 
     @Override
-    public  void create(Object entity)  {
+    public  void create(Object entity) {
         University u;
 
-        try (PreparedStatement ps=connection.prepareStatement
-                ("INSERT INTO Univerities(nameOfUniversity,address,specialization)" +
-                        "VALUES (?,?,?)")){
-            u=(University)entity;
+        try (PreparedStatement ps = connection.prepareStatement
+                ("INSERT INTO Universities(nameOfUniversity,address,specialization)" +
+                        "VALUES (?,?,?)")) {
+            u = (University) entity;
             String nameOfUniversity = u.getNameOfUniversity();
             String address = u.getAddress();
             String specialization = u.getSpecialization();
-            ps.setString(1,nameOfUniversity);
-            ps.setString(2,address);
-            ps.setString(3,specialization);
+            ps.setString(1, nameOfUniversity);
+            ps.setString(2, address);
+            ps.setString(3, specialization);
             ps.executeUpdate();
 
-        } catch (SQLException | ClassCastException  e) {
+        } catch (SQLException | ClassCastException e) {
             e.printStackTrace();
         }
-    }
-}
+    }}
