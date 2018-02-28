17466939740e63a5f507e978b69cfa8fb2e0b8e9
Yura <yuraaaaa87@gmail.com>
2018-02-28 22:18:18 +0200

    added connectionFactory

---
+++ autosalon/src/main/java/com/courses/tellus/autosalon/config/ConnectionFactory.java
@@ -0,0 +1,47 @@
+package com.courses.tellus.autosalon.config;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.h2.jdbcx.JdbcDataSource;
+
+public final class ConnectionFactory {
+
+    private static ConnectionFactory connFactory;
+    private static JdbcDataSource dataSource;
+
+    private ConnectionFactory() {
+    }
+
+    /**
+     * Method to produce singleton config factory.
+     *
+     * @return ConnectionFactory instance.
+     */
+    public static ConnectionFactory getInstance() throws IOException {
+        synchronized (connFactory) {
+            if (connFactory == null) {
+               connFactory = new ConnectionFactory();
+               final Properties dbProperties = new Properties();
+               dbProperties.load(ClassLoader.getSystemResourceAsStream("config.properties"));
+               dataSource = new JdbcDataSource();
+               dataSource.setURL(dbProperties.getProperty("jdbc.url"));
+               dataSource.setUser(dbProperties.getProperty("jdbc.user"));
+               dataSource.setPassword(dbProperties.getProperty("jdbc.pass"));
+           }
+        }
+        return connFactory;
+    }
+
+    /**
+     *Returns new config.
+     *
+     * @return Connection.
+     * @throws SQLException exception.
+     */
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
