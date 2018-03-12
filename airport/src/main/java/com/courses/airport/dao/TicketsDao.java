3b4dc162b6100ac0d08ca80a1c8963c0160fc1b8
dmytro <dvkaminskii@gmail.com>
2018-03-12 19:59:28 +0200

    Creation of interface IAirport and adaptation of TicketsDao class

--- airport/src/main/java/com/courses/airport/dao/TicketsDao.java
+++ airport/src/main/java/com/courses/airport/dao/TicketsDao.java
@@ -9,12 +9,14 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import com.courses.airport.connection.ConnectionFactory;
 import com.courses.airport.exception.DaoException;
 import com.courses.airport.model.Ticket;
 import org.apache.log4j.Logger;
 
-public class TicketsDao {
+public class TicketsDao implements IAirportDao<Ticket>{
     private static final int INDEX_NAME = 1;
     private static final int INDEX_SURNAME = 2;
     private static final int INDEX_FLIGHT_DATE = 3;
@@ -24,8 +26,14 @@ public class TicketsDao {
 
     private final transient Connection connection;
 
-    public TicketsDao(final Connection connection) {
-        this.connection = connection;
+    public TicketsDao(final ConnectionFactory connectionFactory) throws DaoException {
+        try {
+            this.connection = connectionFactory.getConnection();
+        } catch (SQLException e) {
+            final String message = "Connection to database failed! Reason:" + e.getMessage();
+            LOGGER.error(message);
+            throw new DaoException(message, e);
+        }
     }
 
     /**
@@ -35,17 +43,13 @@ public class TicketsDao {
      * @return true if ticket data was inserted to database
      * @throws DaoException if insert operation fails with SQLException
      */
-    public boolean insert(final Ticket ticket) throws DaoException {
-        if (ticket == null) {
-            throw new DaoException("Ticket must be not null!");
-        }
+    public Integer insert(final Ticket ticket) throws DaoException {
         final String sql = "INSERT INTO"
                 + " airport_tickets (name, surname, dateOfFlight, destCity, id)"
                 + " VALUES (?, ?, ?, ?, ?)";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             setStatementValues(statement, ticket);
-            final int rowsInserted = statement.executeUpdate();
-            return rowsInserted > 0;
+            return statement.executeUpdate();
         } catch (SQLException e) {
             final String message = "Insertion failed! Reason: " + e.getMessage();
             LOGGER.error(message);
@@ -60,17 +64,14 @@ public class TicketsDao {
      * @return ticket from database
      * @throws DaoException if find operation fails with SQLException
      */
-    public Ticket findById(final long ticketId) throws DaoException {
-        if (ticketId < 0) {
-            throw new DaoException("Ticket id must be positive, but entered: " + ticketId);
-        }
+    public Optional<Ticket> getById(final Long ticketId) throws DaoException {
         final String sql = "SELECT * FROM airport_tickets WHERE id=" + ticketId;
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
             if (resultSet.next()) {
-                return ticketsFromResultSet(resultSet);
+                return Optional.of(ticketsFromResultSet(resultSet));
             }
-            return null;
+            return Optional.empty();
         } catch (SQLException e) {
             final String message = "Selection failed! Reason: " + e.getMessage();
             LOGGER.error(message);
@@ -84,7 +85,7 @@ public class TicketsDao {
      * @return List of customers
      * @throws DaoException if find operation fails with SQLException
      */
-    public List<Ticket> findAll() throws DaoException {
+    public List<Ticket> getAll() throws DaoException {
         final String sql = "SELECT * FROM airport_tickets";
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
@@ -107,17 +108,13 @@ public class TicketsDao {
      * @return true if operation was success
      * @throws DaoException if remove operation fails with SQLException
      */
-    public boolean update(final Ticket ticket) throws DaoException {
-        if (ticket == null) {
-            throw new DaoException("Ticket must be not null!");
-        }
+    public Integer update(final Ticket ticket) throws DaoException {
         final String sql = "UPDATE airport_tickets "
                 + "SET name=?,surname=?,dateOfFlight=?,destCity=?"
                 + "WHERE id=?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             setStatementValues(statement, ticket);
-            final int rowsAffected = statement.executeUpdate();
-            return rowsAffected == 1;
+            return statement.executeUpdate();
         } catch (SQLException e) {
             final String message = "Update failed! Reason: " + e.getMessage();
             LOGGER.error(message);
@@ -132,14 +129,10 @@ public class TicketsDao {
      * @return true if customer was removed from database
      * @throws DaoException if remove operation fails with SQLException
      */
-    public boolean removeById(final long ticketId) throws DaoException {
-        if (ticketId < 0) {
-            throw new DaoException("Ticket id must be positive, but entered: " + ticketId);
-        }
+    public Integer delete(final Long ticketId) throws DaoException {
         final String sql = "DELETE FROM airport_tickets WHERE id=" + ticketId;
         try (Statement statement = connection.createStatement()) {
-            final int rowsAffected = statement.executeUpdate(sql);
-            return rowsAffected == 1;
+            return statement.executeUpdate(sql);
         } catch (SQLException e) {
             final String message = "Remove failed! Reason: " + e.getMessage();
             LOGGER.error(message);
@@ -157,7 +150,7 @@ public class TicketsDao {
     }
 
     private Ticket ticketsFromResultSet(final ResultSet resultSet) throws SQLException {
-        final long ticketId = resultSet.getLong("id");
+        final Long ticketId = resultSet.getLong("id");
         final String name = resultSet.getString("name");
         final String surname = resultSet.getString("surname");
         final Date flightDate = resultSet.getDate("dateOfFlight");
