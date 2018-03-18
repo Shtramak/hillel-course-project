d1e9500dc994364c1c80b0867cc3dcd82105c8cb
Andrii Kovalchuk <shtramak@gmail.com>
2018-03-18 23:01:54 +0200

    modify CustomerDaoUnitTest acc. to last changes in CustomerDao class

--- autosalon/src/test/java/com/courses/tellus/autosalon/dao/springjdbc/СustomerDaoUnitTest.java
+++ autosalon/src/test/java/com/courses/tellus/autosalon/dao/springjdbc/СustomerDaoUnitTest.java
@@ -3,12 +3,14 @@ package com.courses.tellus.autosalon.dao.springjdbc;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.anyVararg;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import javax.sql.DataSource;
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +22,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class СustomerDaoUnitTest {
@@ -45,40 +48,67 @@ public class СustomerDaoUnitTest {
     }
 
     @Test
-    void getAllReturnsListOfCustomers() {
+    void getAllWhenEntryExistsReturnsListWithCustomer() {
         List<Customer> customers = Arrays.asList(REAL_CUSTOMER);
         when(jdbcTemplate.query(anyString(), any(CustomerMapper.class))).thenReturn(customers);
         assertEquals(customers, customerDao.getAll());
     }
 
     @Test
-    void getByIdReturnsCustomerWithCpecifiedId1() throws Exception {
+    void getAllWhenBadConnectionReturnsEmptyList() {
+        when(jdbcTemplate.query(anyString(), any(CustomerMapper.class)))
+                .thenThrow(BadSqlGrammarException.class);
+        assertEquals(Collections.emptyList(), customerDao.getAll());
+    }
+
+    @Test
+    void getByIdWithExistingIdReturnsCustomer() {
         when(jdbcTemplate.queryForObject(anyString(), any(CustomerMapper.class))).thenReturn(REAL_CUSTOMER);
         assertEquals(Optional.of(REAL_CUSTOMER), customerDao.getById(REAL_CUSTOMER.getId()));
     }
 
     @Test
-    void getByIdWhenEmptyResultSetReturnsOptionalEmpty() throws Exception {
+    void getByIdWhenEmptyResultSetReturnsOptionalEmpty() {
         when(jdbcTemplate.queryForObject(anyString(), any(CustomerMapper.class)))
                 .thenThrow(EmptyResultDataAccessException.class);
         assertEquals(Optional.empty(), customerDao.getById(3L));
     }
 
     @Test
-    void updateReturnsNumberOfAffectedRows() {
-        when(jdbcTemplate.update(anyString(), any(Object[].class))).thenReturn(0);
-        assertEquals(Integer.valueOf(0), customerDao.update(REAL_CUSTOMER));
+    void updateWhenEntryExistsReturnsNumberOfAffectedRows() {
+        when(jdbcTemplate.update(anyString(), (Object[]) anyVararg())).thenReturn(1);
+        assertEquals(Integer.valueOf(1), customerDao.update(REAL_CUSTOMER));
+    }
+
+    @Test
+    void updateWhenBadConnectionReturnsMinus1() {
+        when(jdbcTemplate.update(anyString(), (Object[]) anyVararg()))
+                .thenThrow(BadSqlGrammarException.class);
+        assertEquals(Integer.valueOf(-1), customerDao.update(mock(Customer.class)));
     }
 
     @Test
     void deleteReturnsNumberOfAffectedRows() {
-        when(jdbcTemplate.update(anyString(), any(Object[].class))).thenReturn(0);
-        assertEquals(Integer.valueOf(0), customerDao.delete(REAL_CUSTOMER.getId()));
+        when(jdbcTemplate.update(anyString())).thenReturn(1);
+        assertEquals(Integer.valueOf(1), customerDao.delete(REAL_CUSTOMER.getId()));
+    }
+
+    @Test
+    void deleteWhenBadConnectionReturnsMinus1() {
+        when(jdbcTemplate.update(anyString())).thenThrow(BadSqlGrammarException.class);
+        assertEquals(Integer.valueOf(-1), customerDao.delete(REAL_CUSTOMER.getId()));
     }
 
     @Test
     void insertReturnsNumberOfAffectedRows() {
-        when(jdbcTemplate.update(anyString(), any(Object[].class))).thenReturn(0);
+        when(jdbcTemplate.update(anyString(), (Object[]) anyVararg())).thenReturn(0);
+        assertEquals(Integer.valueOf(0), customerDao.insert(REAL_CUSTOMER));
+    }
+
+    @Test
+    void insertWhenBadConnectionReturns0() {
+        when(jdbcTemplate.update(anyString(), (Object[]) anyVararg()))
+                .thenThrow(BadSqlGrammarException.class);
         assertEquals(Integer.valueOf(0), customerDao.insert(REAL_CUSTOMER));
     }
 }
