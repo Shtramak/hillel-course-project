e82650064df4fba109a8b9a61c5f6d2e552369fe
Stanislav Zabramnyi <stas.zabramnyi@gmail.com>
2018-03-19 23:59:45 +0200

    add universityForm.jsp

---
+++ education/src/main/java/com/courses/tellus/servlet/UniversityServlet.java
@@ -0,0 +1,10 @@
+package com.courses.tellus.servlet;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/universities")
+public class UniversityServlet extends HttpServlet {
+
+}
