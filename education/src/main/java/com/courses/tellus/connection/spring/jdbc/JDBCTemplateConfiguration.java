756394bbf187ae9a67d300ea7c33e9e92f5cb4c0
Heryv1m <podalead@gmail.com>
2018-03-19 14:54:09 +0200

    set dataSource parameter from properties

--- education/src/main/java/com/courses/tellus/connection/spring/jdbc/JDBCTemplateConfiguration.java
+++ education/src/main/java/com/courses/tellus/connection/spring/jdbc/JDBCTemplateConfiguration.java
@@ -3,16 +3,19 @@ package com.courses.tellus.connection.spring.jdbc;
 import javax.sql.DataSource;
 
 import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @PropertySource("classpath:properties/db.properties")
 public class JDBCTemplateConfiguration {
 
-    //@Autowired private transient Environment env;
+    @Autowired
+    private transient Environment env;
 
     /**
      * dataSource that create connection to PostgreSQL Database.
@@ -21,9 +24,9 @@ public class JDBCTemplateConfiguration {
     @Bean
     public DataSource pgDataSource() {
         final PGSimpleDataSource pgds = new PGSimpleDataSource();
-        pgds.setUrl("");
-        pgds.setUser("");
-        pgds.setPassword("");
+        pgds.setUrl(env.getProperty("pg.url"));
+        pgds.setUser(env.getProperty("pg.user"));
+        pgds.setPassword(env.getProperty("pg.password"));
         return pgds;
     }
 
