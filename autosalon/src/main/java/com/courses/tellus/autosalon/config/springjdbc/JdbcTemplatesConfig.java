cbdb2f1cfe2dff13125b88099b39000cf40b03d6
Andrii Kovalchuk <shtramak@gmail.com>
2018-03-27 17:19:03 +0300

    add CustomerServletUnitTest. Refactor CustomerServlet. Project builds

--- autosalon/src/main/java/com/courses/tellus/autosalon/config/springjdbc/JdbcTemplatesConfig.java
+++ autosalon/src/main/java/com/courses/tellus/autosalon/config/springjdbc/JdbcTemplatesConfig.java
@@ -1,10 +1,10 @@
 package com.courses.tellus.autosalon.config.springjdbc;
 
-import javax.sql.DataSource;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
 import org.h2.jdbcx.JdbcDataSource;
 import org.h2.tools.RunScript;
@@ -16,19 +16,29 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @Configuration
 @ComponentScan("com.courses.tellus.autosalon.dao.springjdbc")
 public class JdbcTemplatesConfig {
+    /**
+     *
+     * @return dataSource.
+     * @throws SQLException if smth wrong
+     */
     @Bean
     public DataSource embeddedDataSource() throws SQLException {
-        JdbcDataSource dataSource = new JdbcDataSource();
+        final JdbcDataSource dataSource = new JdbcDataSource();
         dataSource.setPassword("sa");
         dataSource.setUser("sa");
         dataSource.setURL("jdbc:h2:file:/home/andrii/Desktop/Project/tellus/autosalon/out/db;AUTO_SERVER=TRUE");
-        Connection connection = dataSource.getConnection();
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        InputStream resourceAsStream = classLoader.getResourceAsStream("test.sql");
+        final Connection connection = dataSource.getConnection();
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        final InputStream resourceAsStream = classLoader.getResourceAsStream("test.sql");
         RunScript.execute(connection, new InputStreamReader(resourceAsStream));
         return dataSource;
     }
 
+    /**
+     *
+     * @param dataSource is dataSource.
+     * @return JdbcTemplate
+     */
     @Bean
     public JdbcTemplate jdbcTemplate(final DataSource dataSource) {
         return new JdbcTemplate(dataSource);
--- autosalon/src/main/java/com/courses/tellus/autosalon/servlet/CustomerServlet.java
+++ autosalon/src/main/java/com/courses/tellus/autosalon/servlet/CustomerServlet.java
@@ -1,15 +1,15 @@
 package com.courses.tellus.autosalon.servlet;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 
 import com.courses.tellus.autosalon.config.springjdbc.JdbcTemplatesConfig;
 import com.courses.tellus.autosalon.dao.springjdbc.CustomerDao;
@@ -19,60 +19,64 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 @WebServlet(urlPatterns = "/customer/*")
 public class CustomerServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String pathInfo = request.getPathInfo();
+    @SuppressWarnings("PMD.NcssCount")
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+        final String pathInfo = request.getPathInfo();
+        request.setCharacterEncoding("UTF-8");
         if (pathInfo.equals("/add")) {
-            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/jsp/addCustomer.jsp");
+            final RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/addCustomer.jsp");
             dispatcher.forward(request, response);
         }
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JdbcTemplatesConfig.class);
-        CustomerDao customerDao = context.getBean(CustomerDao.class);
+        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JdbcTemplatesConfig.class);
+        final CustomerDao customerDao = context.getBean(CustomerDao.class);
         if (pathInfo.equals("/list")) {
-            request.setCharacterEncoding("UTF-8");
-            List<Customer> allCustomersAfter = customerDao.getAll();
+            final List<Customer> allCustomersAfter = customerDao.getAll();
             request.setAttribute("allCustomersAfter", allCustomersAfter);
-            getServletContext().getRequestDispatcher("/WEB-INF/jsp/listCustomers.jsp").forward(request, response);
+            request.getRequestDispatcher("/WEB-INF/jsp/listCustomers.jsp").forward(request, response);
         }
         if (Character.isDigit(pathInfo.charAt(1))) {
-            Long customerId = Long.valueOf(pathInfo.substring(1));
+            final Long customerId = Long.valueOf(pathInfo.substring(1));
             request.setAttribute("customerId", customerId);
-            Optional<Customer> customerOpt = customerDao.getById(customerId);
-            if (customerOpt.isPresent()){
-                Customer customer = customerOpt.get();
+            final Optional<Customer> customerOpt = customerDao.getById(customerId);
+            if (customerOpt.isPresent()) {
+                final Customer customer = customerOpt.get();
                 request.setAttribute("customer", customer);
                 request.getRequestDispatcher("/WEB-INF/jsp/customerById.jsp").forward(request, response);
-            }else{
-                getServletContext().getRequestDispatcher("/WEB-INF/jsp/customerNotFound.jsp").forward(request, response);
+            } else {
+                request.getRequestDispatcher("/WEB-INF/jsp/customerNotFound.jsp").forward(request, response);
             }
         }
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JdbcTemplatesConfig.class);
-        CustomerDao customerDao = context.getBean(CustomerDao.class);
+    protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+        final AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(JdbcTemplatesConfig.class);
+        final CustomerDao customerDao = context.getBean(CustomerDao.class);
         request.setCharacterEncoding("UTF-8");
-        Customer customer = customerFromRequest(request);
+        final Customer customer = customerFromRequest(request);
         customerDao.insert(customer);
         request.setAttribute("customer", customer);
         request.getRequestDispatcher("/WEB-INF/jsp/successfulAdd.jsp").forward(request, response);
     }
 
-    private Customer customerFromRequest(HttpServletRequest request) throws IOException {
-        Long customerId = Long.valueOf(request.getParameter("id"));
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-        String phone = request.getParameter("phone");
-        LocalDate birthday = stringToLocalDate(request.getParameter("birthday"));
-        Double funds = Double.valueOf(request.getParameter("funds"));
+    private Customer customerFromRequest(final HttpServletRequest request) throws IOException {
+        final Long customerId = Long.valueOf(request.getParameter("id"));
+        final String name = request.getParameter("name");
+        final String surname = request.getParameter("surname");
+        final String phone = request.getParameter("phone");
+        final LocalDate birthday = stringToLocalDate(request.getParameter("birthday"));
+        final Double funds = Double.valueOf(request.getParameter("funds"));
         return new Customer(customerId, name, surname, birthday, phone, funds);
     }
 
-    private static LocalDate stringToLocalDate(String stringDate) {
-        String[] dateParameters = stringDate.split("-");
-        Integer year = Integer.valueOf(dateParameters[0]);
-        Integer month = Integer.valueOf(dateParameters[1]);
-        Integer day = Integer.valueOf(dateParameters[2]);
+    private static LocalDate stringToLocalDate(final String stringDate) {
+        final String[] dateParameters = stringDate.split("-");
+        final Integer year = Integer.valueOf(dateParameters[0]);
+        final Integer month = Integer.valueOf(dateParameters[1]);
+        final Integer day = Integer.valueOf(dateParameters[2]);
         return LocalDate.of(year, month, day);
     }
 }
