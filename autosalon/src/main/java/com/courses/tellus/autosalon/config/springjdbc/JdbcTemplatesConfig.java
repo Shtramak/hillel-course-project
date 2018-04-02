4c38ab47364cb5cdf2160af61688864b6b85b4ce
ChaikaVV <chapaleka@gmail.com>
2018-04-02 22:38:42 +0300

    change JdbcTemplateConfig and use DB H2

--- autosalon/src/main/java/com/courses/tellus/autosalon/config/springjdbc/JdbcTemplatesConfig.java
+++ autosalon/src/main/java/com/courses/tellus/autosalon/config/springjdbc/JdbcTemplatesConfig.java
@@ -1,20 +1,11 @@
 package com.courses.tellus.autosalon.config.springjdbc;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 
-import org.h2.jdbcx.JdbcDataSource;
-import org.h2.tools.RunScript;
 import java.io.FileNotFoundException;
 
-import com.courses.tellus.autosalon.dao.AutosalonDaoInterface;
 import com.courses.tellus.autosalon.dao.springjdbc.AutosalonDao;
-import com.courses.tellus.autosalon.model.Autosalon;
-import com.mysql.cj.jdbc.MysqlDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -26,48 +17,18 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 @ComponentScan("com.courses.tellus.autosalon.dao.springjdbc")
 public class JdbcTemplatesConfig {
 
-    @Value("${jdbc.url.mysql}")
-    private transient String url;
-
-    @Value("${jdbc.user.mysql}")
-    private transient String user;
-
-    @Value("${jdbc.pass.mysql}")
-    private transient String password;
-
     /**
      *
      * @return dataSource.
      * @throws SQLException if smth wrong
      */
-    @Bean
-    public DataSource embeddedDataSource() throws SQLException {
-        final JdbcDataSource dataSource = new JdbcDataSource();
-        dataSource.setPassword("sa");
-        dataSource.setUser("sa");
-        dataSource.setURL("jdbc:h2:file:/home/andrii/Desktop/Project/tellus/autosalon/out/db;AUTO_SERVER=TRUE");
-        final Connection connection = dataSource.getConnection();
-        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        final InputStream resourceAsStream = classLoader.getResourceAsStream("test.sql");
-        RunScript.execute(connection, new InputStreamReader(resourceAsStream));
-        return dataSource;
-    }
 
     @Bean
-    public DataSource dataSourceH2()  {
+    public DataSource dataSource()  {
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript("test.sql")
                 .build();
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        final MysqlDataSource mysql = new MysqlDataSource();
-        mysql.setURL(url);
-        mysql.setUser(user);
-        mysql.setPassword(password);
-        return mysql;
     }
 
     /**
