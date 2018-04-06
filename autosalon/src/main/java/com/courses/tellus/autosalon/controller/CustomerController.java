e29bd52a322f7e0103f37a4b175b23858bc95ad7
Andrii Kovalchuk <shtramak@gmail.com>
2018-04-06 17:14:13 +0300

    add controller package and make index.html as a start page

---
+++ autosalon/src/main/java/com/courses/tellus/autosalon/controller/CustomerController.java
@@ -0,0 +1,10 @@
+package com.courses.tellus.autosalon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/autosalon/customer")
+public class CustomerController {
+
+}
