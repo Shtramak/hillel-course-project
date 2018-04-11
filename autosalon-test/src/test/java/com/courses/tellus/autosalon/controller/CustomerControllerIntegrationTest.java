aa9e971e2ab8767c5918b81c7e6f12b5fbcf6177
Andrii Kovalchuk <shtramak@gmail.com>
2018-04-11 22:42:34 +0300

    add CustomerControllerIntegrationTest. No POST method test

---
+++ autosalon-test/src/test/java/com/courses/tellus/autosalon/controller/CustomerControllerIntegrationTest.java
@@ -0,0 +1,99 @@
+package com.courses.tellus.autosalon.controller;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import javax.servlet.ServletContext;
+
+import com.courses.tellus.autosalon.config.springmvc.WebConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ContextConfiguration(classes = WebConfig.class)
+@WebAppConfiguration
+@ExtendWith(SpringExtension.class)
+@Sql("classpath:test.sql")
+public class CustomerControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+        this.mockMvc = builder.build();
+    }
+
+    @Test
+    void checkWebAppContextContainsCustomerController() {
+        ServletContext servletContext = wac.getServletContext();
+        assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+        assertNotNull(wac.getBean("customerController"));
+    }
+
+    @Test
+    void customerListReturnsListCustomersJspPage() throws Exception {
+        this.mockMvc
+                .perform(get("/autosalon/customer/list"))
+                .andDo(print())
+                .andExpect(view().name("listCustomers"));
+    }
+
+    @Sql("classpath:test.sql")
+    @Test
+    void customerByIdWhenCustomerExistsReturnsCustomerByIdJspPage() throws Exception {
+        this.mockMvc
+                .perform(get("/autosalon/customer/1"))
+                .andDo(print())
+                .andExpect(view().name("customerById"));
+    }
+
+    @Test
+    void customerByIdWhenCustomerNotExistsReturnscustomerNotFoundJspPage() throws Exception {
+        this.mockMvc
+                .perform(get("/autosalon/customer/123"))
+                .andDo(print())
+                .andExpect(view().name("customerNotFound"));
+    }
+
+    @Test
+    void deleteByIdWhenCustomerExistsReturnsCustomerByIdJspPage() throws Exception {
+        this.mockMvc
+                .perform(get("/autosalon/customer/delete/1"))
+                .andDo(print())
+                .andExpect(view().name("customerDeletedById"));
+    }
+
+    @Test
+    void deleteByIdWhenCustomerNotExistsReturnscustomerNotFoundJspPage() throws Exception {
+        this.mockMvc
+                .perform(get("/autosalon/customer/delete/123"))
+                .andDo(print())
+                .andExpect(view().name("customerNotFound"));
+    }
+
+    @Test
+    void addCustomerPageReturnsAddCustomerJspPage() throws Exception {
+        this.mockMvc
+                .perform(get("/autosalon/customer/add"))
+                .andDo(print())
+                .andExpect(view().name("addCustomer"));
+    }
+}
