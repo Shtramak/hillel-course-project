3abef389614c61a0924d75b953072774028153c3
Heryv1m <podalead@gmail.com>
2018-04-22 14:26:47 +0300

    change databae for integration test on h2

--- education/src/main/resources/database/h2/init_h2_values.sql
+++ education/src/main/resources/database/h2/init_h2_values.sql
@@ -1,3 +1,3 @@
 INSERT into university(uni_id, name_of_university, address, specialization) values (2, 'KPI', 'peremohy 27 str.', 'technical');
 INSERT into student(student_id, first_name, last_name, address, student_card_number) values (2, 'John', 'Shepard', 'Lvivska 3 str','423541646' );
-INSERT into subject(subject_id, name, description, valid, date_of_creation) values (2, 'Math', 'Science about nums and count', true, DATE ('2000-5-12'));
+INSERT into subject(subject_id, name, description, valid, date_of_creation) values (2, 'Math', 'Science about nums and count', true, TO_DATE('12/05/2000', 'DD/MM/YYYY'));
