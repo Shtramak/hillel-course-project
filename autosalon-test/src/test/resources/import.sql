098d0fcc78a3c0804bd80d410e94660da3bd7a2d
Heryv1m <podalead@gmail.com>
2018-04-27 17:57:13 +0300

    fix autosalon merge - 1

--- autosalon-test/src/test/resources/import.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Autosalon (autosalon_id, name, address, telephone) VALUES (1, 'Bavaria', 'Kyiv', '00001');
-
-INSERT INTO AUTO (auto_id, brand, model, manufact_year, producer_country, PRICE, AUTOSALON_ID) VALUES (1, 'BMW', 'X1', 2013, 'Germany', 20000, 1);
-INSERT INTO AUTO (auto_id, brand, model, manufact_year, producer_country, PRICE, AUTOSALON_ID) VALUES (2, 'BMW', 'X2', 2014, 'Germany', 25000, 1);
-INSERT INTO AUTO (auto_id, brand, model, manufact_year, producer_country, PRICE, AUTOSALON_ID) VALUES (3, 'BMW', 'X3', 2015, 'Germany', 30000, 1);
-INSERT INTO AUTO (auto_id, brand, model, manufact_year, producer_country, PRICE, AUTOSALON_ID) VALUES (4, 'BMW', 'X4', 2016, 'Germany', 35000, 1);
-INSERT INTO AUTO (auto_id, brand, model, manufact_year, producer_country, PRICE, AUTOSALON_ID) VALUES (5, 'BMW', 'X5', 2017, 'Germany', 40000, 1);
-
-
